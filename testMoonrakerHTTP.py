a83d12a69c14ccdb546d23b3d48f7f7b910a4c5d
kallekj <kalle@djervbrant.net>
2021-07-26 01:26:08 +0200

    Test RPC to moonraker is working

---
+++ testMoonrakerHTTP.py
@@ -0,0 +1,44 @@
+#from jsonrpcclient.clients.http_client import HTTPClient
+#import jsonrpclib
+import socket
+import requests
+import json
+
+
+
+
+#class moonrakerClient:
+#	def __init__(self, address, port):
+#		self.client = HTTPClient('%s' % (address))
+
+#class moonrakerClientV2:
+#	def __init__(self, address, port):
+#		self.client = jsonrpclib.Server('%s:%s' % (address, str(port)))
+
+
+def main():
+    #print(jsonrpclib.config.version)
+    #moonraker_rpc_v2 = moonrakerClientV2("192.168.1.166", 7125)
+    #moonraker_rpc_v2.client._request("info", {}, rpcid=1)
+    #moonraker_rpc = moonrakerClient("http://192.168.1.166", 7125)
+    #pinter_obj = moonraker_rpc.client.send('{"jsonrpc": "2.0", "method": "printer.info", "params":{}, "id": 1}')
+    #moonraker = moonrakerSocket("http://192.168.1.166", 7125)
+    #moonraker.s.send("/printer/info")
+    url = "http://192.168.1.166:7125"
+
+    # Example echo method
+    payload = {
+        "method": "printer.info",
+        "params": {},
+        "jsonrpc": "2.0",
+        "id": 1234,
+    }
+    response = requests.get(url, json=payload)
+    print(response)
+
+    response = requests.get(url + "/printer/info").json()
+    print(response)
+
+if __name__ == "__main__":
+    main()
+
--- testMoonrakerRPC.py
+++ testMoonrakerRPC.py
@@ -1,44 +1,17 @@
-from jsonrpcclient.clients.http_client import HTTPClient
-import jsonrpclib
-import socket
+import asyncio
+import websockets
+from jsonrpcclient.clients.websockets_client import WebSocketsClient
 import requests
 import json
 
+def get_token():
+    response = requests.get("http://192.168.1.166:7125/access/oneshot_token").json()
+    print(response["result"])
+    return response["result"]
 
+async def main():
+    async with websockets.connect("ws://192.168.1.166:7125/websocket?token=%s" % get_token()) as ws:
+        response = await WebSocketsClient(ws).request("printer.info")
+    print(response.data.result)
 
-
-class moonrakerClient:
-	def __init__(self, address, port):
-		self.client = HTTPClient('%s' % (address))
-
-class moonrakerClientV2:
-	def __init__(self, address, port):
-		self.client = jsonrpclib.Server('%s:%s' % (address, str(port)))
-
-
-def main():
-    #print(jsonrpclib.config.version)
-    #moonraker_rpc_v2 = moonrakerClientV2("192.168.1.166", 7125)
-    #moonraker_rpc_v2.client._request("info", {}, rpcid=1)
-    #moonraker_rpc = moonrakerClient("http://192.168.1.166", 7125)
-    #pinter_obj = moonraker_rpc.client.send('{"jsonrpc": "2.0", "method": "printer.info", "params":{}, "id": 1}')
-    #moonraker = moonrakerSocket("http://192.168.1.166", 7125)
-    #moonraker.s.send("/printer/info")
-    url = "http://192.168.1.166:7125"
-
-    # Example echo method
-    payload = {
-        "method": "printer.info",
-        "params": {},
-        "jsonrpc": "2.0",
-        "id": 1234,
-    }
-    response = requests.get(url, json=payload)
-    print(response)
-
-    response = requests.get(url + "/printer/info")
-    print(response)
-
-if __name__ == "__main__":
-    main()
-
+asyncio.get_event_loop().run_until_complete(main())
